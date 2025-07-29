a9a1c4b5ea1b1f0f144c3929b6d9bce56e9d15b6
Marco Gelli <marco.gelli@3di.it>
2025-07-29 14:36:50 +0200

    Add truncate functions for Cronos and POA tables: implement logic to clear PostgreSQL tables for respective services.

---
+++ src/cronos.py
@@ -0,0 +1,30 @@
+import logging
+
+from utils import ETLContext, truncate_pg_table
+
+CRONOS_TABLES = [
+    "cronos_companies",
+    "cronos_physical_structures",
+    "cronos_plan_specialties",
+    "cronos_plan_specialty_aliases",
+    "cronos_plan_grouping_specialties",
+    "cronos_plans",
+    "cronos_taxonomies",
+    "dm70_taxonomies",
+    "healthcare_companies",
+]
+
+
+def truncate_cronos_tables(ctx: ETLContext) -> None:
+    """
+    Truncate all the tables in the PostgreSQL database of A.Re.A. Cronos service.
+
+    Parameters
+    ----------
+    ctx : ETLContext
+        The ETL context containing database connections
+    """
+    logging.info(f"Truncating all target tables in PostgreSQL {ctx.pg_engine_cronos}...")
+
+    for table in CRONOS_TABLES:
+        truncate_pg_table(ctx.pg_engine_cronos, table)
---
+++ src/poa.py
@@ -0,0 +1,37 @@
+import logging
+
+from utils import ETLContext, truncate_pg_table
+
+POA_TABLES = [
+    "areas",
+    "function_diagram_nodes",
+    "function_diagrams",
+    "legal_inquiries",
+    "legal_inquiries_types",
+    "models",
+    "node_types",
+    "nodes",
+    "notifications",
+    "organigram_attachments",
+    "organigram_emails",
+    "organigrams",
+    "parameters",
+    "rule_types",
+    "rules",
+    "sub_areas",
+]
+
+
+def truncate_poa_tables(ctx: ETLContext) -> None:
+    """
+    Truncate all the tables in the PostgreSQL database of A.Re.A. POA service.
+
+    Parameters
+    ----------
+    ctx : ETLContext
+        The ETL context containing database connections
+    """
+    logging.info(f"Truncating all target tables in PostgreSQL {ctx.pg_engine_poa}...")
+
+    for table in POA_TABLES:
+        truncate_pg_table(ctx.pg_engine_poa, table)
